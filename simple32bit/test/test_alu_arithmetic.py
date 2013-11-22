63d9d2f2f3c7c90ef7b7df75e8ff4ca52ea3f863
Sven-Hendrik Haase <sh@lutzhaase.com>
2013-11-22 01:56:08 +0100

    Proper project setup, set up testing framework

---
+++ simple32bit/test/test_alu_arithmetic.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from myhdl import Simulation
+
+class TestAluArithmetic(TestCase):
+    def testAdd(self):
+        """Check that the ALU can correctly execute an add operation."""
+        #Simulation()
+        self.assertEqual(True, True)

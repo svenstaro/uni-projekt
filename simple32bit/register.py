041f329ffadfa1c6497d3680cb29367618ba2be1
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-11-28 16:47:53 +0100

    add register.py

---
+++ simple32bit/register.py
@@ -0,0 +1,13 @@
+from myhdl import *
+
+def register(clk, reset, we, data_in, data_out):
+    data = Signal(intbv(0)[16:])
+
+    @always_seq(clk.posedge, reset=reset)
+    def logic():
+        if we:
+            data.next = data_in
+        data_out.next = data
+
+    return logic
+

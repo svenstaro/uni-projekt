e070561679ee01fe254b65e0958f4243e8273ed7
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-11-29 10:06:08 +0100

    changed import path in unittesting

--- scripts/run-assembler-tests.py
+++ scripts/run-assembler-tests.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python2.7
 
+import os
 import unittest
 
 loader = unittest.TestLoader()
-testsuite = loader.discover("assembler")
+testsuite = loader.discover(os.path.dirname(__file__) + "/../assembler")
 unittest.TextTestRunner(verbosity=2).run(testsuite)

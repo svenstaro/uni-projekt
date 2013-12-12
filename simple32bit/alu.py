67ffd252cc1eeea52121dbc397ee22925f61db96
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-13 00:00:31 +0100

    alu fix + improvements

--- simple32bit/alu.py
+++ simple32bit/alu.py
@@ -1,16 +1,15 @@
 from math import log
 from myhdl import *
 
-def alu(clk, opc, upS, A, B, Cin, Res, Z, N, C, V):
+def alu(opc, ups, A, B, Cin, Res, Z, N, C, V):
     """This represents the ALU of the microcontroller.
 
     All Parameters are Signals as usual.
 
-    clk -- The clock
     opc -- The opcode (must be a valid alu opcode)
-    upS -- Signal which says if to update the status flags
-    A   -- First input
-    B   -- Second input
+    ups -- Signal which says if to update the status flags
+    A   -- First input - must be modbv
+    B   -- Second input - must be modbv
     Cin -- Carry input
     Res -- The Result
     Z   -- Zero flag
@@ -20,12 +19,16 @@ def alu(clk, opc, upS, A, B, Cin, Res, Z, N, C, V):
     """
     def ADD(A, B, Cin):
         return A + B
-    def ADDC(A, B, Cin):
+    def ADC(A, B, Cin):
         return A + B + Cin
     def SUB(A, B, Cin):
         return A - B
     def SBC(A, B, Cin):
-        return A - B - C
+        return A - B - Cin
+    def RSB(A, B, Cin):
+        return B - A
+    def RSC(A, B, Cin):
+        return B - A - Cin
 
     def MUL(A, B, Cin):
         return A * B #TODO
@@ -39,7 +42,7 @@ def alu(clk, opc, upS, A, B, Cin, Res, Z, N, C, V):
     def XOR(A, B, Cin):
         return A ^ B
     def NOT(A, B, Cin):
-        return ~B #TODO
+        return ~B
 
     def LSL(A, B, Cin):
         return A << B
@@ -51,40 +54,40 @@ def alu(clk, opc, upS, A, B, Cin, Res, Z, N, C, V):
         return 0 #TODO
 
 
-    opcodes = { 
+    opcodes = {
                 #arithmetic
-                0b000000 : ADD,
-                0b000001 : ADC,
-                0b000100 : SUB,
-                0b000101 : SBC,
-                0b000110 : RSB,
-                0b000111 : RSC,
+                0b0000 : ADD,
+                0b0001 : ADC,
+                0b0100 : SUB,
+                0b0101 : SBC,
+                0b0110 : RSB,
+                0b0111 : RSC,
                 #arithm special
-                0b000010 : MUL,
-                0b000011 : DIV,
+                0b0010 : MUL,
+                0b0011 : DIV,
                 #logic
-                0b001000 : AND,
-                0b001001 : ORR,
-                0b001010 : XOR,
-                0b001011 : NOT,
+                0b1000 : AND,
+                0b1001 : ORR,
+                0b1010 : XOR,
+                0b1011 : NOT,
                 #shift/rotate
-                0b001100 : LSL,
-                0b001101 : ASR,
-                0b001110 : LSR,
-                0b001111 : ROR
+                0b1100 : LSL,
+                0b1101 : ASR,
+                0b1110 : LSR,
+                0b1111 : ROR
               }
 
-    @always_seq(clk.posedge)
+    @always_comb
     def logic():
-        assert opc+0 in opcodes #dirty hack
+        assert int(opc) in opcodes
 
-        Res.next = opcodes[opc](A, B, Cin)
-        
-        if upS: #update status flags
-            Z.next = Res.next == 0
-            N.next = Res.next[math.log(Res.next.max, 2) - 1]
-            C.next = 0 #TODO
-            V.next = 0 #TODO 
+        Res.next = opcodes[int(opc)](A, B, Cin)
+
+        if ups: #update status flags
+            Z.next = result == 0
+            N.next = result < 0
+            C.next = False #TODO
+            V.next = False #TODO
 
     return logic
 

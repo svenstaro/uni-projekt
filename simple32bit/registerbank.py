fd3cc82200063208a7fde8b70785fe4a6abd64ef
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-12 15:15:40 +0100

    add async read to registerbank

--- simple32bit/registerbank.py
+++ simple32bit/registerbank.py
@@ -4,16 +4,23 @@ def registerbank(clk, reset, we, channel, data_in, data_out, amount = 16, bitwid
     reg_data = [Signal(intbv(0)[bitwidth:]) for _ in range(1, amount)]
 
     @always_seq(clk.posedge, reset=reset)
-    def logic():
+    def write():
         assert channel < amount
 
-        if channel == 0: #zero register is always zero!
-            data_out.next = 0
-        else:
+        if channel != 0: #zero register is always zero!
             if we:
                 reg_data[channel-1].next = data_in
 
             data_out.next = reg_data[channel-1]
 
-    return logic
+    @always_comb
+    def read():
+        assert channel < amount
+
+        if channel == 0:
+            data_out.next = 0
+        else:
+            data_out.next = reg_data[channel-1]
+
+    return write, read
 

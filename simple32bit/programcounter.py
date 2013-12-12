59d897e084ecf7f12119df83690616ce1d370d6c
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-12 14:49:01 +0100

    add first implm of programcounter
    tests sind noch nötig

---
+++ simple32bit/programcounter.py
@@ -0,0 +1,42 @@
+from myhdl import *
+
+def programcounter(clk, reset, enabled, a, b, c, d, select1, select2, out):
+    """Represents the programcounter (PC)
+
+    clk              -- The clock
+    reset            -- A reset input
+    enabled          -- enabled input
+    a, b, c, d       -- data input
+    select1, select2 -- mux selector
+    out              -- data out
+
+    s2 s1 out
+    --+--+---
+     0| 0| a
+    --+--+---
+     0| 1| b
+    --+--+---
+     1| 0| c
+    --+--+---
+     1| 1| d
+    """
+    
+    data = Signal(intbv(0)[16:])
+
+    @always_comb(clk.posedge, reset=reset)
+    def logic():
+        if enabled:
+            if   not s2 and not s1:
+                data.next = a
+            elif not s2 and     s1:
+                data.next = b
+            elif     s2 and not s1:
+                data.next = c
+            else:
+                data.next = d
+
+        out.next = data
+
+    return logic
+
+

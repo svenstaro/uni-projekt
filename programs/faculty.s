25b6a9fd8c928b775fe2e25df9b4f616b1b023bc
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2013-12-16 12:28:38 +0100

    programs: Added faculty and fibonacci

---
+++ programs/faculty.s
@@ -0,0 +1,20 @@
+jmp .start
+
+;.globl faculty
+.faculty:
+; $1 - unsigned int
+movs $2, $1
+mov $1, #1
+jz $15
+.fac_loop:
+mul $1, $1, $2
+subs $2, $2, #1
+jnz .fac_loop
+ret
+
+
+.start:
+mov $1, #6
+call .faculty
+swi #1 ; should be 720
+halt

e3aa86caddbd148c31ebe54aa12080cfafa5ce5a
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2013-12-17 21:28:03 +0100

    Assembler: Labels may not start with '-'

--- assembler/tools.py
+++ assembler/tools.py
@@ -6,7 +6,7 @@ def tobin(value, width):
         return bin(value & ((1 << width) - 1))[2:].rjust(width, "1")
     return bin(value)[2:].zfill(width)[-width:]
 
-labelPattern = re.compile("^(?P<label>[a-zA-Z._-][a-zA-Z0-9._-]*)$")
+labelPattern = re.compile("^(?P<label>[a-zA-Z._][a-zA-Z0-9._-]*)$")
 
 
 def label2immediate(arg, state):

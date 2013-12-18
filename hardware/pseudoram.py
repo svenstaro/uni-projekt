575b0e2e2bb2e2cb6f97e5e1f6d36c8dc90ef56b
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-18 15:11:28 +0100

    add assert to pseudoram

--- hardware/pseudoram.py
+++ hardware/pseudoram.py
@@ -1,16 +1,19 @@
 from myhdl import *
 
-def pram(clk, we, addr, data_in, data_out, depth=128):
-    
+def pseudoram(clk, we, addr, data_in, data_out, depth=128):
     mem = [Signal(intbv(0)[32:]) for _ in range(128)]
 
     @always(clk.posegde)
     def write():
+        assert addr >= len(mem)
+
         if we:
             mem[addr].next = data_in
 
     @always_comb
     def read():
+        assert addr < len(mem)
+
         data_out.next = mem[addr]
 
     return write, read

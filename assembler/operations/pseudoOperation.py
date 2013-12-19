73d2d72c6f1d8ce46e003b1425a3cd40bf041a75
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2013-12-19 17:03:58 +0100

    Assembler: added nop pseudo-instruction

--- assembler/operations/pseudoOperation.py
+++ assembler/operations/pseudoOperation.py
@@ -1,8 +1,9 @@
-from operations import Operation, JumpOperation
+from operations import Operation, JumpOperation, TwoOpAluOperation
 
 
 class PseudoOperation(Operation):
     opcodes = {"ret": JumpOperation.fromText("jmp $15", None).binary,
-               "halt": JumpOperation.fromText("jmp #0", None).binary}
+               "halt": JumpOperation.fromText("jmp #0", None).binary,
+               "nop": TwoOpAluOperation.fromText("mov $0, $0", None).binary}
     argTypes = []
 

50905b67e92c91d7a5fdf4b9659b4c9ac6687c10
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-20 11:12:32 +0100

    add first impl of IR-decoder

---
+++ hardware/decoder.py
@@ -0,0 +1,34 @@
+from myhdl import *
+
+def decoder(ir, aluop, dest, source, op1, op2, source2, imm24, imm16, statusUp):
+    """ 
+        This is the IR decoder
+        All inputs are Signals as usual
+
+    ir (I32)        -- The instruction itself
+    aluop (O4)      -- The decoded aluop
+    dest (O4)       -- The destination register
+    source (O4)     -- The first source register
+    op1 (bool)      -- Whether source or imm24 is active
+    op2 (bool)      -- Wheter source2 or imm16 is active
+    source2 (O4)    -- The second source register
+    imm24 (O24)     -- The decoded 24bit width imm
+    imm16 (O16)     -- The decoded 16bit width imm (for alu)
+    statusUp (bool) -- True if the status bit should be updated
+
+    """
+    
+    @always_comb
+    def logic():
+        aluop.next      = ir[30:26]
+        dest.next       = ir[25:21]
+        source.next     = ir[21:17]
+        op1.next        = ir[25]
+        op2.next        = ir[16]
+        source2.next    = ir[16:12]
+        imm24.next      = ir[25:0]
+        imm16.next      = ir[16:0]
+        statsUp         = ir[25]
+
+    return logic
+

ba075111ee4663515fbcb71e627c125c920ac163
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-20 16:05:09 +0100

    'fixed' register{,bank}
    das lesen passiert nun wieder asynchron, so wie sich das gehört!

--- hardware/register.py
+++ hardware/register.py
@@ -4,10 +4,13 @@ def register(clk, reset, we, data_in, data_out, bitwidth=32):
     data = Signal(intbv(0)[bitwidth:])
 
     @always_seq(clk.posedge, reset)
-    def logic():
+    def write():
         if we:
             data.next = data_in
+
+    @always_comb
+    def read():
         data_out.next = data
 
-    return logic
+    return write, read
 
--- hardware/registerbank.py
+++ hardware/registerbank.py
@@ -4,15 +4,21 @@ def registerbank(clk, we, addrx, addry, addrz, xout, yout, zin, amount = 16, bit
     reg_data = [Signal(intbv(0)[bitwidth:]) for _ in range(1, amount)]
 
     @always(clk.posedge)
-    def logic():
+    def write():
         assert addrx < amount
         assert addry < amount
         assert addrz < amount
 
-        xout.next = 0 if addrx == 0 else reg_data[addrx-1]
-        yout.next = 0 if addry == 0 else reg_data[addry-1]
-
         if we and addrz != 0:
             reg_data[addrz-1].next = zin[bitwidth:]
 
-    return logic
+    @always_comb
+    def read():
+        assert addrx < amount
+        assert addry < amount
+        assert addry < amount
+
+        xout.next = 0 if addrx == 0 else reg_data[addrx-1]
+        yout.next = 0 if addry == 0 else reg_data[addry-1]
+
+    return write, read

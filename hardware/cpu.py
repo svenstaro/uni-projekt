2c30da57d424dc01c83a1436858933863d9b5378
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-20 19:09:10 +0100

    add first things of cpu

--- hardware/cpu.py
+++ hardware/cpu.py
@@ -1,7 +1,9 @@
 from myhdl import *
 
-def cpu(clk, nextA, nextB, upcmux2, upcmux1, upcmux0, rxBuf, addrx15, enIr, addrBuf, aluBuf, enZ, enN, enC, enV, enReg, 
-        pcBuf, enPC, pcmux1, pcmux0, enMRR, enMDR, enMAR, MRRbuf, MDRbuf, mWe, mOe):
+def cpu(clk, nextA, nextB, upcmux2, upcmux1, upcmux0, addrx15,
+        rxBuf, addrBuf, aluBuf, pcBuf
+        enIr, enZ, enN, enC, enV, enReg, enPC, pcmux1, pcmux0,
+        enMRR, enMDR, enMAR, MRRbuf, MDRbuf, mWe, mOe):
 
     """
         This is a cpu state maschine. Let's see how far we come.
@@ -12,18 +14,19 @@ def cpu(clk, nextA, nextB, upcmux2, upcmux1, upcmux0, rxBuf, addrx15, enIr, addr
         nextB (O8)      -- next uPC addr B
         upcmux2 (Obool) -- uPCMuxSelector
         upcmux1 (Obool) -- uPCMuxSelector
-        upsmux0 (Obool) -- uPCMuxSelector
-        rxBuf (Obool)   -- bufbit for xout
+        upcmux0 (Obool) -- uPCMuxSelector
         addrx15 (Obool) -- set addrx for register to 15 (return addr register)
-        enIr (Obool)    -- enable IR
+        rxBuf (Obool)   -- bufbit for xout
         addrBuf (Obool) -- bufbit for addr (pc+imm)
         aluBuf (Obool)  -- bufbit for alu
+        pcBuf (Obool)   -- bufbit for pc (programcounter)
+        enIr (Obool)    -- enable IR
         enZ (Obool)     -- enable zero-register
         enN (Obool)     -- enable negative-register
         enC (Obool)     -- enable carry-register
         enV (Obool)     -- enable overflow register
         enReg (Obool)   -- enable registe write
-        pcBuf (Obool)   -- bufbit for pc (programcounter)
+        enPC (0bool)    -- enable ProgrammCounter
         pcmux1 (Obool)  -- pcMuxSelector
         pcmux0 (Obool)  -- pcMuxSelector
         enMRR (Obool)   -- enable MemoryReadRegister
@@ -35,4 +38,21 @@ def cpu(clk, nextA, nextB, upcmux2, upcmux1, upcmux0, rxBuf, addrx15, enIr, addr
         mOe (Obool)     -- memoryOutputEnable
     """
 
-    pass
+    tState = enum('RESET', 'FETCH', 'DECODE', 'EXECUTE', 'ALUOP', 'HALT') #TODO add more
+    state = tState.RESET
+
+    def logic():
+        if   state == RESET:
+            pass
+        elif state == FETCH:
+            pass
+        elif state == DECODE:
+            pass
+        elif state == EXECUTE:
+            pass
+        elif state == ALUOP:
+            pass
+        elif state == HALT:
+            pass
+
+    return logic

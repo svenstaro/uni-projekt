708f09edaf68ec0a5769f4068870914de90334dc
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-21 13:32:40 +0100

    next impl step of cpu

--- hardware/cpu.py
+++ hardware/cpu.py
@@ -1,44 +1,45 @@
+# -*- coding: utf-8 -*-
 from myhdl import *
 
-def cpu(clk, nextA, nextB, upcmux2, upcmux1, upcmux0, addrx15,
-        rxBuf, addrBuf, aluBuf, pcBuf
-        enIr, enZ, enN, enC, enV, enPC, regWe pcmux1, pcmux0,
+def cpu(clk, addr, nextA, nextB, upcmux2, upcmux1, upcmux0, addrx15,
+        rxBuf, addrBuf, aluBuf, pcBuf,
+        enIr, enZ, enN, enC, enV, enPC, regWe, pcmux1, pcmux0,
         enMRR, enMDR, enMAR, MRRbuf, MDRbuf, mWe, mOe):
-
     """
         This is a cpu state maschine. Let's see how far we come.
         All parameters are Signals as usual.
 
-        clk (Ibool)     -- The clock
-        nextA (O8)      -- next uPC addr A
-        nextB (O8)      -- next uPC addr B
+        clk     (Ibool) -- The clock
+        addr    (I8)    -- Next action
+        nextA   (O8)    -- next uPC addr A
+        nextB   (O8)    -- next uPC addr B
         upcmux2 (Obool) -- uPCMuxSelector
         upcmux1 (Obool) -- uPCMuxSelector
         upcmux0 (Obool) -- uPCMuxSelector
         addrx15 (Obool) -- set addrx for register to 15 (return addr register)
-        rxBuf (Obool)   -- bufbit for xout
+        rxBuf   (Obool) -- bufbit for xout
         addrBuf (Obool) -- bufbit for addr (pc+imm)
-        aluBuf (Obool)  -- bufbit for alu
-        pcBuf (Obool)   -- bufbit for pc (programcounter)
-        enIr (Obool)    -- enable IR
-        enZ (Obool)     -- enable zero-register
-        enN (Obool)     -- enable negative-register
-        enC (Obool)     -- enable carry-register
-        enV (Obool)     -- enable overflow register
-        enPC (0bool)    -- enable ProgrammCounter
-        regWe (Obool)   -- enable registe write
-        pcmux1 (Obool)  -- pcMuxSelector
-        pcmux0 (Obool)  -- pcMuxSelector
-        enMRR (Obool)   -- enable MemoryReadRegister
-        enMDR (Obool)   -- enable MemoryDataRegister
-        enMAR (Obool)   -- enable MemoryAddressRegister
-        MRRbuf (Obool)  -- bufbit MemoryReadRegister
-        MDRbuf (Obool)  -- bufbit MemoryDataRegister
-        mWe (Obool)     -- memoryWriteEnable
-        mOe (Obool)     -- memoryOutputEnable
+        aluBuf  (Obool) -- bufbit for alu
+        pcBuf   (Obool) -- bufbit for pc (programcounter)
+        enIr    (Obool) -- enable IR
+        enZ     (Obool) -- enable zero-register
+        enN     (Obool) -- enable negative-register
+        enC     (Obool) -- enable carry-register
+        enV     (Obool) -- enable overflow register
+        enPC    (Obool) -- enable ProgrammCounter
+        regWe   (Obool) -- enable registe write
+        pcmux1  (Obool) -- pcMuxSelector
+        pcmux0  (Obool) -- pcMuxSelector
+        enMRR   (Obool) -- enable MemoryReadRegister
+        enMDR   (Obool) -- enable MemoryDataRegister
+        enMAR   (Obool) -- enable MemoryAddressRegister
+        MRRbuf  (Obool) -- bufbit MemoryReadRegister
+        MDRbuf  (Obool) -- bufbit MemoryDataRegister
+        mWe     (Obool) -- memoryWriteEnable
+        mOe     (Obool) -- memoryOutputEnable
     """
 
-    tState = enum('ILLEGAL', 'FETCH', 'DECODE', 'ALUOP', 'HALT') #TODO add more
+    tState = enum('UNKNOWN', 'FETCH', 'DECODE', 'ALUOP', 'JUMP', 'LOAD', 'STORE', 'ILLEGAL', 'HALT')  # TODO add more
     state = tState.FETCH
 
     """
@@ -79,14 +80,27 @@ def cpu(clk, nextA, nextB, upcmux2, upcmux1, upcmux0, addrx15,
     def logic():
         preset()
 
+        if state == tState.UNKNOWN:
+            if   addr[32:30] == 0b00:
+                state = tState.ALUOP
+            elif addr[32:30] == 0b11:
+                state = tState.JUMP
+            elif addr[32:28] == 0b100:
+                state = tState.LOAD
+            elif addr[32:28] == 0b101:
+                state = tState.STORE
+            else:
+                state = tState.ILLEGAL # TODO add more
+
         if   state == tState.FETCH:
             pcBuf.next = True
-            enMar.next = True
+            enMAR.next = True
             yield clk.posedge
             preset()
             mOe.next = True
-            yield clk.posedge #TODO add delay for timing
+            yield clk.posedge # TODO add delay for timing
             preset()
+            mOe.next = True
             enMRR.next = True
             yield clk.posedge
             preset()

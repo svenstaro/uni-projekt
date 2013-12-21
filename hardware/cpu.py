cfca0df1b457c050abd3bd8b7f722d593a5290ab
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-21 02:04:31 +0100

    first impl of few cpu operation

--- hardware/cpu.py
+++ hardware/cpu.py
@@ -2,7 +2,7 @@ from myhdl import *
 
 def cpu(clk, nextA, nextB, upcmux2, upcmux1, upcmux0, addrx15,
         rxBuf, addrBuf, aluBuf, pcBuf
-        enIr, enZ, enN, enC, enV, enReg, enPC, pcmux1, pcmux0,
+        enIr, enZ, enN, enC, enV, enPC, regWe pcmux1, pcmux0,
         enMRR, enMDR, enMAR, MRRbuf, MDRbuf, mWe, mOe):
 
     """
@@ -25,8 +25,8 @@ def cpu(clk, nextA, nextB, upcmux2, upcmux1, upcmux0, addrx15,
         enN (Obool)     -- enable negative-register
         enC (Obool)     -- enable carry-register
         enV (Obool)     -- enable overflow register
-        enReg (Obool)   -- enable registe write
         enPC (0bool)    -- enable ProgrammCounter
+        regWe (Obool)   -- enable registe write
         pcmux1 (Obool)  -- pcMuxSelector
         pcmux0 (Obool)  -- pcMuxSelector
         enMRR (Obool)   -- enable MemoryReadRegister
@@ -38,21 +38,75 @@ def cpu(clk, nextA, nextB, upcmux2, upcmux1, upcmux0, addrx15,
         mOe (Obool)     -- memoryOutputEnable
     """
 
-    tState = enum('RESET', 'FETCH', 'DECODE', 'EXECUTE', 'ALUOP', 'HALT') #TODO add more
-    state = tState.RESET
+    tState = enum('ILLEGAL', 'FETCH', 'DECODE', 'ALUOP', 'HALT') #TODO add more
+    state = tState.FETCH
 
+    """
+        Ich muss noch überlegen, wie ich den bedingten sprung hier rein bekomme.
+        Entweder ich switsche nach einer eingangsvariable oder son kack, oder ich
+        führe einen upc ein (wie er zur zeit halb vorgesehen ist, aber keine ahnung wie)
+    """
+
+    def preset():
+        nextA.next = 0
+        nextB.next = 0
+        upcmux2.next = False
+        upcmux1.next = False
+        upcmux0.next = False
+        addrx15.next = False
+        rxBuf.next = False
+        addrBuf.next = False
+        aluBuf.next = False
+        pcBuf.next = False
+        enIr.next = False
+        enZ.next = False
+        enN.next = False
+        enC.next = False
+        enV.next = False
+        regWe.next = False
+        enPC.next = False
+        pcmux1.next = False
+        pcmux0.next = False
+        enMRR.next = False
+        enMDR.next = False
+        enMAR.next = False
+        MRRbuf.next = False
+        MDRbuf.next = False
+        mWe.next = False
+        mOe.next = False
+
+    @always(clk.posedge)
     def logic():
-        if   state == RESET:
-            pass
-        elif state == FETCH:
-            pass
-        elif state == DECODE:
-            pass
-        elif state == EXECUTE:
-            pass
-        elif state == ALUOP:
-            pass
-        elif state == HALT:
+        preset()
+
+        if   state == tState.FETCH:
+            pcBuf.next = True
+            enMar.next = True
+            yield clk.posedge
+            preset()
+            mOe.next = True
+            yield clk.posedge #TODO add delay for timing
+            preset()
+            enMRR.next = True
+            yield clk.posedge
+            preset()
+            MRRbuf.next = True
+            enIR.next = True
+            state = tState.DECODE
+        elif state == tState.DECODE:
+            upcmux2.next = True
+            upcmux1.next = True
+            upcmux0.next = True
+            enPC.next    = True
+        elif state == tState.ALUOP:
+            enZ.next = True
+            enN.next = True
+            enC.next = True
+            enV.next = True
+            aluBuf.next = True
+            regWe.next  = True
+            state = tState.FETCH
+        elif state == tState.HALT:
             pass
 
     return logic

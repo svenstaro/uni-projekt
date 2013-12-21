06667200adf24e4ca8278fb31db00638a5146b7d
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-21 12:11:31 +0100

    refactored alu
    myhdl kann das dict nicht umwandeln, ergo ist es jetzt ein riesiges
    if/elif

--- hardware/alu.py
+++ hardware/alu.py
@@ -1,4 +1,3 @@
-from math import log
 from myhdl import *
 
 def alu(opc, ups, A, B, Cin, Res, Z, N, C, V, bitwidth=32):
@@ -18,71 +17,42 @@ def alu(opc, ups, A, B, Cin, Res, Z, N, C, V, bitwidth=32):
     V   (Obool) -- Overflow flag
     """
 
-    def ADD(A, B, Cin):
-        return A + B
-    def ADC(A, B, Cin):
-        return A + B + Cin
-    def SUB(A, B, Cin):
-        return A - B
-    def SBC(A, B, Cin):
-        return A - B - Cin
-    def RSB(A, B, Cin):
-        return B - A
-    def RSC(A, B, Cin):
-        return B + Cin - A
-
-    def MUL(A, B, Cin):
-        return A * B #TODO hardware nutzen
-    def DIV(A, B, Cin):
-        return A // B #TODO rauswerfen
-
-    def AND(A, B, Cin):
-        return A & B
-    def ORR(A, B, Cin):
-        return A | B
-    def XOR(A, B, Cin):
-        return A ^ B
-    def NOT(A, B, Cin):
-        return ~B
-
-    def LSL(A, B, Cin):
-        return (A << B)[bitwidth:].signed()
-    def ASR(A, B, Cin):
-        return A >> B
-    def LSR(A, B, Cin):
-        return ((A[bitwidth+1:] & (2**bitwidth-1)) >> B)[bitwidth:].signed() #TODO unhuebsch
-    def ROR(A, B, Cin):
-        return A << (bitwidth - B)
-
-
-    opcodes = {
-                #arithmetic
-                0b0000 : ADD,
-                0b0001 : ADC,
-                0b0100 : SUB,
-                0b0101 : SBC,
-                0b0110 : RSB,
-                0b0111 : RSC,
-                #arithm special
-                0b0010 : MUL,
-                0b0011 : DIV,
-                #logic
-                0b1000 : AND,
-                0b1001 : ORR,
-                0b1010 : XOR,
-                0b1011 : NOT,
-                #shift/rotate
-                0b1100 : LSL,
-                0b1101 : ASR,
-                0b1110 : LSR,
-                0b1111 : ROR
-              } #TODO umschreiben nach if/elif
-
     @always_comb
     def logic():
-        assert int(opc) in opcodes
+        result = 0
 
-        result = opcodes[int(opc)](A, B, Cin)
+        if   opc == 0b000:
+            result =  A + B
+        elif opc == 0b001:
+            result = A + B + Cin
+        elif opc == 0b0100:
+            result = A - B
+        elif opc == 0b0101:
+            result = A - B - Cin
+        elif opc == 0b0110:
+            result = B - A
+        elif opc == 0b0111:
+            result = B + Cin - A
+        elif opc == 0b0010:
+            result = A * B  #TODO hardware nutzen
+        elif opc == 0b0011:
+            result = A // B #TODO rauswerfen
+        elif opc == 0b1000:
+            result = A & B
+        elif opc == 0b1001:
+            result = A | B
+        elif opc == 0b1010:
+            result = A ^ B
+        elif opc == 0b1011:
+            result = ~B
+        elif opc == 0b1100:
+            result = (A << B)[bitwidth:].signed()
+        elif opc == 0b1101:
+            result = A >> B
+        elif opc == 0b1110:
+            result = ((A[bitwidth+1:] & (2**bitwidth-1)) >> B)[bitwidth:].signed() #TODO unhuebsch
+        elif opc == 0b1111:
+            result = A << (bitwidth - B)
 
         if ups: #update status flags
             amsb = A[bitwidth-1]

2cf14567715a3972102004893101af1108609b81
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2013-12-22 00:19:53 +0100

    Programs: gcd: Optimized + new example

--- programs/gcd.s
+++ programs/gcd.s
@@ -8,7 +8,8 @@ gcd:
 ; $1 < 0 => $1 = -$1
 cmp $1, #0
 jgt .gcd_cont1
-mul $1, $1, #-1
+not $1, $1
+add $1, $1, #1
 
 
 ; $2 == 0 => ggt = $1
@@ -30,13 +31,13 @@ sub $2, $2, $1
 jmp gcd
 
 .a:
-.word 999
+.word 150
 .b:
-.word 111
+.word 26
 
 .start:
 ld $1, .a
 ld $2, .b
 call gcd
-swi #1 ; should be 9
+swi #1 ; should be 2
 halt

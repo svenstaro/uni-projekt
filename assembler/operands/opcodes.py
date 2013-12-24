8c3d87eca056b6d075aecb88f08040457d23afcf
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2013-12-24 01:03:08 +0100

    Assembler: Reenabled disassembler

--- assembler/operands/opcodes.py
+++ assembler/operands/opcodes.py
@@ -10,5 +10,9 @@ class OpcodesBaseClass(Ignore):
     def isValidBinary(cls, arg):
         return arg in cls.mapping.values()
 
+    @classmethod
+    def fromBinary(cls, arg, state):
+        return (key for (key,value) in cls.mapping.items() if value == arg).next()
+
 def Opcodes(mapping):
     return type("Opcodes", (OpcodesBaseClass,), dict(size=len(mapping.values()[0]), mapping=mapping))

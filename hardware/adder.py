d0d2caac64d86eb6e50503d77b329d0322ebac84
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-25 13:46:16 +0100

    added addc to adder.py

--- hardware/adder.py
+++ hardware/adder.py
@@ -16,3 +16,22 @@ def adder(A, B, S):
         S.next = A + B
 
     return logic
+
+def adderC(A, B, cin, S, cout, bitwidth=32):
+    """Simple fulladder with carryin and carryout
+
+    All parameters are Signals as usual
+
+    A (I)   -- First input
+    B (I)   -- Second input
+    cin     -- Carry in
+    S (O)   -- Result A+B
+    cout    -- Carry out
+    """
+
+    @always_comb
+    def logic():
+        S.next = A + B
+        cout.next = intbv(A + B)[bitwidth]
+
+    return logic

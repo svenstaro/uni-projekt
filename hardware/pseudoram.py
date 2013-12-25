46345f6ec75eea3be38597f42ca93b948b833e83
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-25 18:04:11 +0100

    add cable select to pseudor{a,o}m

--- hardware/pseudoram.py
+++ hardware/pseudoram.py
@@ -1,21 +1,20 @@
 from myhdl import *
 
-def pseudoram(clk, we, oe, addr, data_in, data_out, mem=[Signal(intbv(0)[32:]) for _ in range(128)]):
+def pseudoram(clk, we, oe, cs, addr, data_in, data_out, mem=[Signal(intbv(0)[32:]) for _ in range(128)]):
     """This is a pseudoram
     """
 
     @always(clk.posegde)
     def write():
         assert addr < len(mem)
-
-        if we:
+        if cs and we:
             mem[addr].next = data_in
 
     @always_comb
     def read():
         assert addr < len(mem)
 
-        if oe:
+        if cs and oe:
             data_out.next = mem[addr]
 
     return write, read

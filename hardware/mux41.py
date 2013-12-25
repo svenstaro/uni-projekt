f0daf95040590b0ba3699fb218a880936c920eba
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-25 13:26:58 +0100

    redesigned mux41

--- hardware/mux41.py
+++ hardware/mux41.py
@@ -18,13 +18,15 @@ def mux41(s1, s0, i00, i01, i10, i11, out):
     """
     @always_comb
     def logic():
-        if   not s1 and not s0:
-            out.next = i00
-        elif not s1 and     s0:
-            out.next = i01
-        elif     s1 and not s0:
-            out.next = i10
+        if not s1:
+            if not s0:
+                out.next = i00
+            else:
+                out.next = i01
         else:
-            out.next = i11
+            if not s0:
+                out.next = i10
+            else:
+                out.next = i11
 
     return logic

f04f63d902d20e416d88251be1eecd6f30647a57
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-26 22:25:48 +0100

    'fixed' alu
    inputs are now signed, instead of unsigned

--- hardware/alu.py
+++ hardware/alu.py
@@ -19,39 +19,42 @@ def alu(opc, A, B, Cin, Res, Z, N, C, V, bitwidth=32):
     @always_comb
     def logic():
         result = 0
+        a = A.signed()
+        b = B.signed()
 
         if   opc == 0b0000: #ADD
-            result = A + B
+            result = a + b
         elif opc == 0b0001: #ADC
-            result = A + B + Cin
+            result = a + b + Cin
         elif opc == 0b0100: #SUB
-            result = A - B
+            result = a - b
         elif opc == 0b0101: #SBC
-            result = A - B - Cin
+            result = a - b - Cin
         elif opc == 0b0110: #RSB
-            result = B - A
+            result = b - a
         elif opc == 0b0111: #RSC
-            result = B + Cin - A
+            result = b + Cin - a
         elif opc == 0b0010: #MUL
-            result = A * B  #TODO hardware nutzen
+            result = a * b  #TODO hardware nutzen
         elif opc == 0b0011: #ADN
-            result = A & ~B
+            result = a & ~b
         elif opc == 0b1000: #AND
-            result = A & B
+            result = a & b
         elif opc == 0b1001: #ORR
-            result = A | B
+            result = a | b
         elif opc == 0b1010: #XOR
-            result = A ^ B
+            result = a ^ b
         elif opc == 0b1011: #NOT
-            result = A | ~B
+            result = a | ~b
         elif opc == 0b1100: #LSL
-            result = (A << B)[bitwidth:]
+            result = a << b
         elif opc == 0b1101: #ASR
-            result = A >> B
+            result = a >> b
         elif opc == 0b1110: #LSR
-            result = ((A[bitwidth+1:] & (2**bitwidth-1)) >> B)[bitwidth:] #TODO unhuebsch
+            result = ((intbv(a)[bitwidth+1:] & (2**bitwidth-1)) >> b)[bitwidth:] #TODO unhuebsch
         elif opc == 0b1111: #ROR
-            result = A << (bitwidth - B)
+            result = a << (bitwidth + ~b + 1)
+            #pass
 
         amsb = A[bitwidth-1]
         bmsb = B[bitwidth-1]

ee6a8fbd5f89537da7382f0c044670677a50eea8
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2013-12-28 18:01:49 +0100

    Assembler: Take -4 offset into account for ld/str/adr instructions

--- assembler/operations/jumpOperation.py
+++ assembler/operations/jumpOperation.py
@@ -1,5 +1,5 @@
 from .operation import Operation
-from operands import JumpLabelOperand, Opcodes
+from operands import LabelOperand, Opcodes
 
 
 class JumpOperation(Operation):
@@ -32,4 +32,4 @@ class JumpOperation(Operation):
         opcodes["j" + name] = "01" + code
 
 
-    structure = [Opcodes(opcodes), JumpLabelOperand]
+    structure = [Opcodes(opcodes), LabelOperand]

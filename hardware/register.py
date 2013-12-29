a4c850ac81c07df5c05c5b8fed12d45a86976b51
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-29 01:07:42 +0100

    renamed register→registerr because of reserved keywords in VHDL

--- hardware/register.py
+++ hardware/register.py
@@ -1,15 +1,15 @@
 from myhdl import *
 
-def register(clk, reset, we, data_in, data_out, bitwidth=32):
+def registerr(clk, reset, we, din, dout, bitwidth=32):
     data = Signal(intbv(0)[bitwidth:])
 
     @always_seq(clk.posedge, reset)
     def write():
         if we:
-            data.next = data_in
+            data.next = din
 
     @always_comb
     def read():
-        data_out.next = data
+        dout.next = data
 
     return write, read

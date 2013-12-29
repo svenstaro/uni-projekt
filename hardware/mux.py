794c5fdeb8ff4e39be95de25a6a269e33c776da1
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-29 01:03:13 +0100

    renamed parameter in mux

--- hardware/mux.py
+++ hardware/mux.py
@@ -1,6 +1,6 @@
 from myhdl import *
 
-def mux41(s1, s0, i00, i01, i10, i11, out):
+def mux41(s1, s0, i00, i01, i10, i11, res):
     """Simple 4:1 multiplexer
 
     All parameters are Signals as usual
@@ -11,7 +11,7 @@ def mux41(s1, s0, i00, i01, i10, i11, out):
     i01 (I)     -- second input
     i10 (I)     -- third input
     i11 (I)     -- fourth input
-    out (O)     -- output
+    res (O)     -- output
 
     the output is defined by the selector and the specific output.
     all Is and O must have the same bitlength
@@ -20,19 +20,19 @@ def mux41(s1, s0, i00, i01, i10, i11, out):
     def logic():
         if not s1:
             if not s0:
-                out.next = i00
+                res.next = i00
             else:
-                out.next = i01
+                res.next = i01
         else:
             if not s0:
-                out.next = i10
+                res.next = i10
             else:
-                out.next = i11
+                res.next = i11
 
     return logic
 
 
-def mux21(s, A, B, out):
+def mux21(s, A, B, res):
     """Simple 2:1 multiplexer
 
     All parameters are Signals as usual
@@ -40,17 +40,14 @@ def mux21(s, A, B, out):
     s0  (Ibool) -- selector
     A   (I)     -- first input
     B   (I)     -- second input
-    out (O)     -- output
+    res (O)     -- output
 
     the output is defined by the selector and the specific output.
     all Is and O must have the same bitlength
     """
     @always_comb
     def logic():
-        if not s:
-            out.next = A
-        else:
-            out.next = B
+        res.next = B if s else A
 
     return logic
 

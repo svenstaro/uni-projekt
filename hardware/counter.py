f2b701d263e16f74eb79498bf093e3784cae4ec9
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-30 18:55:13 +0100

    fixed counter: intbv->modbv

--- hardware/counter.py
+++ hardware/counter.py
@@ -4,7 +4,7 @@ from register import *
 
 def counter(clk, reset, enable, data_out, bitwidth=32):
 
-    sig = Signal(intbv(1)[bitwidth:])
+    sig = Signal(modbv(1)[bitwidth:])
 
     add = adder(Signal(intbv(1)[bitwidth:]), data_out, sig)
     data = registerr(clk, reset, enable, sig, data_out)

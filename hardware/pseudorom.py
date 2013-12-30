df52c12a8d2498d5a5ac308e42749238970a9a15
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-30 20:42:33 +0100

    add debug information to pseudorom

--- hardware/pseudorom.py
+++ hardware/pseudorom.py
@@ -1,4 +1,5 @@
 from myhdl import *
+from assembler.assembler import getTextOfCommand
 
 def pseudorom(oe, cs, addr, dout, mem):
     """This is a pseudorom
@@ -8,7 +9,11 @@ def pseudorom(oe, cs, addr, dout, mem):
     def read():
         if cs and oe:
             assert int(addr)//4 < len(mem)
-            
+
+            if __debug__:
+                a = bin(mem[int(addr)//4], width=32)
+                print "ROM (" + '0x%02X' % addr + "): " + ' '.join(map(lambda *xs: ''.join(xs), *[iter(a)]*8)) + ' | ' + getTextOfCommand(a)
+
             dout.next = mem[int(addr)//4]
 
     return read

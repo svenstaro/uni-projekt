6b4a8d4001ad518b4081c7eb09c6e02413bdc83d
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-30 19:00:14 +0100

    fixed jumpunit test

--- hardware/test/test_jumpunit.py
+++ hardware/test/test_jumpunit.py
@@ -28,11 +28,27 @@ def genSim(verifyMethod, cl=DutClass, clkfreq=1, trace=False):
 
 
 class JumpUnitTest(TestCase):
-    def testSomething(self):
+    def testJumpUnit(self):
         def verify(cl, dut):
-            for _ in range(200):
-                cl.C.next = not cl.C
+            assert isinstance(cl, DutClass)
+
+            tests = [(0b00000, 0,0,0,0, 0),
+                     (0b00001, 0,0,0,0, 1),
+                     (0b00010, 0,0,0,0, 0),
+                     (0b00010, 0,0,0,1, 1),
+                     (0b00011, 0,0,0,0, 1),
+                     (0b00011, 0,0,0,1, 0),
+                    ]
+
+            for t in tests:
+                cl.code.next = t[0]
+                cl.Z.next = t[1]
+                cl.N.next = t[2]
+                cl.C.next = t[3]
+                cl.V.next = t[4]
                 yield delay(1)
+                self.assertEquals(t[5], cl.out, msg="%s != %s %s %s %s %s %s" % (cl.out, bin(t[0],width=5),t[1],t[2],t[3],t[4],t[5]))
+
 
         genSim(verify).run()
 

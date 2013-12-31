1fab7091f32fe5f468474101b6029ec3852697e9
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2013-12-31 02:04:09 +0100

    pushing of imm24 is now available
    create issue (Felix, fix das mal!)

--- assembler/operations/__init__.py
+++ assembler/operations/__init__.py
@@ -4,8 +4,8 @@ from .memOperation import MemOperation
 from .jumpOperation import JumpOperation
 from .pseudoOperation import PseudoOperations
 from .swiOperation import SwiOperation
-from .stackOperation import StackOperation
+from .stackOperation import PushOperation, PopOperation
 
 
 def getOperations():
-    return PseudoOperations() + [AluOperation, AluSOperation, JumpOperation, MemOperation, SwiOperation, StackOperation]
+    return PseudoOperations() + [AluOperation, AluSOperation, JumpOperation, MemOperation, SwiOperation, PushOperation, PopOperation]
--- assembler/operations/stackOperation.py
+++ assembler/operations/stackOperation.py
@@ -1,9 +1,13 @@
 from .operation import Operation
-from operands import Zero, Register, Opcodes
+from operands import Zero, Register, Opcodes, Operand2
 
 
-class StackOperation(Operation):
-    opcodes = {"push": "1110000",
-               "pop":  "1110100"}
+class PopOperation(Operation):
+    opcodes = {"pop":  "1110100"}
 
     structure = [Opcodes(opcodes), Zero(21), Register]
+
+class PushOperation(Operation):
+    opcodes = {"push" : "1110000"}
+
+    structure = [Opcodes(opcodes), Operand2(25)]

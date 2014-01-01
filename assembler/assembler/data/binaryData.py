49f09746500634f21de6b3de9c916950741f58d4
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2014-01-01 13:41:08 +0100

    Assembler: Allow unsigned data instructions

--- assembler/assembler/data/binaryData.py
+++ assembler/assembler/data/binaryData.py
@@ -1,19 +1,27 @@
 from .data import Data
 from ..errors import EncodingError
 from .. import tools
+import ast
 
 
 class BinaryData(Data):
 
     @classmethod
     def fromText(cls, arg, state):
+        ex = EncodingError(arg, "is not a valid %s" % cls.__name__)
         if not cls.isValidText(arg):
-            raise EncodingError(arg, "is not a valid %s" % cls.__name__)
+            raise ex
         data = arg[len(cls.start):]
-        number = str(tools.label2immediate(data, state) + state.position) if tools.labelPattern.match(data) else data
-        binary = tools.immediate2binary(number, cls.size)
-        if not binary:
-            raise EncodingError(arg, "is not a valid %s" % cls.__name__)
+        if tools.labelPattern.match(data):
+            number = tools.label2immediate(data, state) + state.position
+	else:
+            try:
+                number = ast.literal_eval(data)
+            except SyntaxError:
+                raise ex
+        if not -2 ** (cls.size - 1) <= number <= 2 ** cls.size - 1:
+            raise ex
+        binary = tools.tobin(number, cls.size)
         return cls(arg, binary)
 
     @classmethod

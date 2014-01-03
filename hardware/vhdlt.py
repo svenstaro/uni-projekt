935f62e94fc932dea20f336d3278d5da528746e7
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2014-01-03 02:28:47 +0100

    ...

--- hardware/vhdlt.py
+++ hardware/vhdlt.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
             yield cl.clk.posedge
             if cl.bus == 0b01000011111111111111111111111100: #halt
                 raise StopSimulation("HALT DETECTED (%s)" % now())
-            elif (not (cl.bus._val is None)) and cl.bus[32:26] == 0b111110: #swi
+            elif cl.bus._val is not None and cl.bus[32:26] == 0b111110: #swi
                 yield cl.clk.posedge
                 yield cl.clk.posedge
                 yield cl.clk.negedge

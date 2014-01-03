ecae3372dd8f754e05c35d613e8efb00eed27285
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2014-01-03 02:28:32 +0100

    add push_pop example

---
+++ programs/test_pushpop.s
@@ -0,0 +1,31 @@
+ld $14, .sp
+mov $1, $14
+push #34
+push #42
+sub $2, $1, $14
+pop $1
+pop $1
+
+mov $1, #0
+.loop_beg:
+    push $1
+    swi #1
+    add $1, $1, #1
+    cmp $1, #16
+    jgt .loop_end
+    jmp .loop_beg
+
+.loop_end:
+
+.loopo_beg:
+    pop $1
+    swi #1
+    subs $1, $1, #1
+    jlt .loopo_end
+    jmp .loopo_beg
+.loopo_end:
+
+halt
+
+.sp:
+    .word 0x80000100

925305aeaa09e50ef021340b43aea3d046ca7243
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2014-01-19 00:04:09 +0100

    improved asm editor

--- debugger/asmeditor.py
+++ debugger/asmeditor.py
@@ -9,63 +9,96 @@
 from PyQt4 import Qsci, QtCore, QtGui
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-from PyQt4.Qsci import QsciScintilla, QsciLexerCustom
+from PyQt4.Qsci import *
+
+from assembler import isLabel
 
 
 # TODO make lexer work
-class LexerAsm(Qsci.QsciLexerCustom):
+class LexerAsm(QsciLexerCustom):
     def __init__(self, obj = None):
-        Qsci.QsciLexerCustom.__init__(self, obj)
-        self.sci = None
-        self.plainFont = QtGui.QFont()
-        self.plainFont.setPointSize(10)
-        self.plainFont.setFamily('Courier')
-        self.marginFont = QtGui.QFont()
-        self.marginFont.setPointSize(10)
-        self.marginFont.setFamily('Courier')
-        self.boldFont = QtGui.QFont()
-        self.boldFont.setPointSize(10)
-        self.boldFont.setFamily('Courier')
-        self.boldFont.setBold(True)
-        self.styles = [
-            Qsci.QsciStyle(0, QtCore.QString("mov"), QtGui.QColor("#000000"), QtGui.QColor("#ffffff"), self.plainFont, True),
-            Qsci.QsciStyle(1, QtCore.QString("add"), QtGui.QColor("#008000"), QtGui.QColor("#eeffee"), self.marginFont, True),
-            Qsci.QsciStyle(2, QtCore.QString("jmp"), QtGui.QColor("#000080"), QtGui.QColor("#ffffff"), self.boldFont, True),
-            Qsci.QsciStyle(3, QtCore.QString("string"), QtGui.QColor("#800000"), QtGui.QColor("#ffffff"), self.marginFont, True),
-            Qsci.QsciStyle(4, QtCore.QString("atom"), QtGui.QColor("#008080"), QtGui.QColor("#ffffff"), self.plainFont, True),
-            Qsci.QsciStyle(5, QtCore.QString("macro"), QtGui.QColor("#808000"), QtGui.QColor("#ffffff"), self.boldFont, True),
-            Qsci.QsciStyle(6, QtCore.QString("error"), QtGui.QColor("#000000"), QtGui.QColor("#ffd0d0"), self.plainFont, True)]
+        QsciLexerCustom.__init__(self, obj)
+        self._styles = {
+            0: 'Default',
+            1: 'Comment',
+            2: 'Instruction',
+            3: 'Register',
+            4: 'Immediate',
+            5: 'Label',
+            6: 'Data'
+        }
+        for key, value in self._styles.iteritems():
+            setattr(self, value, key)
 
-    def description(self, ix):
-        for i in self.styles:
-            if i.style() == ix:
-                return QtCore.QString(i.description())
-        return QtCore.QString("")
+    def description(self, style):
+        return self._styles.get(style, '')
 
-    def setEditor(self, sci):
-        self.sci = sci
-        Qsci.QsciLexerCustom.setEditor(self, sci)
+    def defaultColor(self, style):
+        if style == self.Default:
+            return QtGui.QColor('#000000')
+        elif style == self.Comment:
+            return QtGui.QColor('#C0C0C0')
+        elif style == self.Instruction:
+            return QtGui.QColor('#FF8000')
+        elif style == self.Register:
+            return QtGui.QColor('#00C0C0')
+        elif style == self.Immediate:
+            return QtGui.QColor('#00CC00')
+        elif style == self.Label:
+            return QtGui.QColor('#ABCDEF')
+        elif style == self.Data:
+            return QtGui.QColor('#FEDCBA')
+        return Qsci.QsciLexerCustom.defaultColor(self, style)
+
+    def defaultPaper(self, style):
+        return QtGui.QColor('#FFFFFF')
 
     def styleText(self, start, end):
-        lines = self.getText(start, end)
-        offset = start
-        self.startStyling(offset, 0)
-        print self.getText(start, end)
-        for i in lines:
-            if i == "":
-                self.setStyling(1, self.styles[0])
-                offset += 1
-                continue
-            if i[0] == '%':
-                self.setStyling(len(i)+1, self.styles[1])
-                offset += len(i)+1
-                continue
-            self.setStyling(len(i)+1, self.styles[0])
-            offset += len(i)+1
+        editor = self.editor()
+        if editor is None:
+            return
 
-    def getText(self, start, end):
-        data = self.sci.text()
-        return data[start:end].split('\n')
+        # scintilla works with encoded bytes, not decoded characters.
+        # this matters if the source contains non-ascii characters and
+        # a multi-byte encoding is used (e.g. utf-8)
+        source = ''
+        if end > editor.length():
+            end = editor.length()
+        if end > start:
+            source = bytearray(end - start)
+            editor.SendScintilla(editor.SCI_GETTEXTRANGE, start, end, source)
+        if not source:
+            return
+
+        set_style = self.setStyling
+        self.startStyling(start, 0x1f)
+        # scintilla always asks to style whole lines
+        for line in source.splitlines(False):
+            # XXX: insert here!
+            posInLine = 0
+            while posInLine < len(line):
+                state = self.Default
+                length = len(line)
+                if not line:
+                    pass
+                elif isLabel(line):
+                    state = self.Label
+                elif line.startswith('.'):
+                    state = self.Data
+                else:
+                    # the following will style lines like "x = 0"
+                    pos = line.find('=')
+                    if pos > 0:
+                        set_style(pos, self.Key)
+                        set_style(1, self.Assignment)
+                        length = length - pos - 1
+                        state = self.Value
+                    else:
+                        state = self.Default
+                set_style(length, state)
+                posInLine += length
+
+            set_style(1, self.Default) # for the newline, don't forget this!
 
 
 class AsmEditor(QsciScintilla):
@@ -103,9 +136,9 @@ class AsmEditor(QsciScintilla):
 
         # set asm lexer
         # TODO does not work yet :/
-        lexer = LexerAsm()
-        lexer.setDefaultFont(font)
-        self.setLexer(lexer)
+        self.lexer = LexerAsm(self)
+        self.lexer.setDefaultFont(font)
+        self.setLexer(self.lexer)
         self.SendScintilla(QsciScintilla.SCI_STYLESETFONT, 1, 'Courier')
 
         # Don't want to see the horizontal scrollbar at all

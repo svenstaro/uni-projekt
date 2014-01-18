67078e994d592d5837fe04aef64c27b3f128fc18
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2014-01-18 12:05:27 +0100

    fixed gui...

--- debugger/debugger_ui.py
+++ debugger/debugger_ui.py
@@ -4,23 +4,21 @@ import sys
 from PyQt4 import QtGui, uic
 from PyQt4.QtCore import *
 from debugger import Debugger
+from asmeditor import AsmEditor
 import debugger_rc
-
 
 class MainWindow(QtGui.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.debugger = None
-        #self.ramui = uic.loadUi('ram.ui', QtGui.QDialog())
+
         self.ui = uic.loadUi('gui.ui', self)
 
         self.ui.actionOpen.triggered.connect(self.openNewFile)
 
-        self.show()
-
     @pyqtSlot()
     def openNewFile(self):
-        filename = QtGui.QFileDialog.getOpenFileNameAndFilter(self, 'Open File', filter="Compiled/Debug Files (*.s.out.dbg, *.s.out) (*.s.out);;All Files (*)")[0]
+        filename = QtGui.QFileDialog.getOpenFileNameAndFilter(self, 'Open File', filter="Compiled/Debug Files (*.out.dbg *.s.out) (*.out.dbg *.out);;All Files (*)")[0]
         if filename != '':
             self.debugger = Debugger.loadFromFile(filename)
             with open(str(filename).rsplit('.', 1)[0]) as content:
@@ -49,4 +47,5 @@ class MainWindow(QtGui.QMainWindow):
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     window = MainWindow()
+    window.show()
     sys.exit(app.exec_())
---
+++ debugger/ramviewer.py
@@ -0,0 +1,4 @@
+from PyKate4.okteta import Okteta
+
+class RamViewer(Okteta.ByteArrayColumnView):
+    pass

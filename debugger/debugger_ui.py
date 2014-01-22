9775c293de90d138758c3dd3bb9c7b0cc7908b85
Marcel Hellwig <1hellwig@informatik.uni-hamburg.de>
2014-01-22 18:47:57 +0100

    removed rom + other fancy things

--- debugger/debugger_ui.py
+++ debugger/debugger_ui.py
@@ -12,42 +12,50 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.debugger = None
-        self.decoder = AsmDecoder()
         self.ui = uic.loadUi('gui.ui', self)
 
         self.ui.editor.setReadOnly(True)
-        self.ui.rom.setDecoder(self.decoder)
-        self.ui.rom.setReadOnly(True)
-        self.ui.rom.setOverwriteMode(False)
-        self.ui.rom.setNoOfBytesPerLine(4)
 
-        self.ui.editor.breakpointSet.connect(self.breakpointset)
-        self.ui.editor.breakpointRemoved.connect(self.breakpointremoved)
-        self.ui.ram.dataChanged.connect(self.ramChanged)
-        self.ui.actionOpen.triggered.connect(self.openNewFile)
-        self.ui.actionRun.triggered.connect(self.run)
-        self.ui.actionStep_Over.triggered.connect(self.stepOver)
-        self.ui.actionStep_Into.triggered.connect(self.stepInto)
+        # nearly (all) connects are done via qtdesigner
+        self.ui.editor.breakpointSet.connect(self.breakpointSet)
+        self.ui.editor.breakpointRemoved.connect(self.breakpointRemoved)
 
     def updateDisplay(self):
-        for i in range(1, 15):
-            getattr(self.ui, "reg_"+str(i)).setText(str(self.debugger.register[i]))
-        self.ui.ram.setData(''.join(map(chr, self.debugger.ram)))
-        self.ui.Z.setState(self.debugger.Z)
-        self.ui.N.setState(self.debugger.N)
-        self.ui.C.setState(self.debugger.C)
-        self.ui.V.setState(self.debugger.V)
-        self.ui.editor.setPcLine(self.debugger.getContentLine(self.debugger.pc))
+        if self.debugger:
+            for i in range(1, 15):
+                getattr(self.ui, "reg_"+str(i)).setText(str(self.debugger.register[i]))
+            self.ui.ram.setData(''.join(map(chr, self.debugger.ram)))
+            self.ui.Z.setState(self.debugger.Z)
+            self.ui.N.setState(self.debugger.N)
+            self.ui.C.setState(self.debugger.C)
+            self.ui.V.setState(self.debugger.V)
+            self.ui.editor.setPcLine(self.debugger.getContentLine(self.debugger.pc))
+        else:
+            for i in range(1, 15):
+                getattr(self.ui, "reg_"+str(i)).setText('0')
+            self.ui.ram.setData('')
+            self.ui.Z.setState(False)
+            self.ui.N.setState(False)
+            self.ui.C.setState(False)
+            self.ui.V.setState(False)
+            self.ui.editor.setPcLine(0)
 
     @pyqtSlot()
     def openNewFile(self):
-        filename = QFileDialog.getOpenFileNameAndFilter(self, 'Open File', filter="Compiled/Debug Files (*.out.dbg *.s.out) (*.out.dbg *.out);;All Files (*)")[0]
+        filename = QFileDialog.getOpenFileName(self, 'Open File', filter="Compiled/Debug Files (*.out.dbg *.s.out) (*.out.dbg *.out);;All Files (*)")
         if filename != '':
-            self.debugger = Debugger.loadFromFile(str(filename), memorysize=0x20)
-            self.ui.rom.setData(''.join(map(chr, self.debugger.rom)))
+            self.debugger = Debugger.loadFromFile(str(filename), memorysize=1024*1024)
             self.ui.ram.setData(''.join(map(chr, self.debugger.ram)))
             self.ui.editor.setText(self.debugger.fileContent())
             self.ui.editor.resetMarker()
+        self.updateDisplay()
+
+    @pyqtSlot()
+    def closeFile(self):
+        self.debugger = None
+        self.ui.ram.setData('')
+        self.ui.editor.setText('')
+        self.ui.editor.resetMarker()
         self.updateDisplay()
 
     @pyqtSlot(int)
@@ -63,6 +71,12 @@ class MainWindow(QMainWindow):
             self.updateDisplay()
 
     @pyqtSlot()
+    def reset(self):
+        if self.debugger:
+            self.debugger.reset()
+            self.updateDisplay()
+
+    @pyqtSlot()
     def stepOver(self):
         if self.debugger:
             self.debugger.stepOver()
@@ -75,17 +89,23 @@ class MainWindow(QMainWindow):
             self.updateDisplay()
 
     @pyqtSlot(int)
-    def breakpointset(self, line):
+    def breakpointSet(self, line):
         if self.debugger:
             self.debugger.breakpoints.append(self.debugger.getRomAddr(line))
 
     @pyqtSlot(int)
-    def breakpointremoved(self, line):
+    def breakpointRemoved(self, line):
         if self.debugger:
             self.debugger.breakpoints.remove(self.debugger.getRomAddr(line))
 
+    def contextMenuEvent(self, QContextMenuEvent):
+        #disable rightclick menu
+        pass
+
 
 if __name__ == '__main__':
+    import gc
+    gc.set_debug(gc.DEBUG_COLLECTABLE | gc.DEBUG_INSTANCES | gc.DEBUG_COLLECTABLE)
     app = QApplication(sys.argv)
     MainWindow().show()
     sys.exit(app.exec_())
--- debugger/memoryviewer.py
+++ debugger/memoryviewer.py
@@ -4,17 +4,3 @@ from assembler import getTextOfCommand
 class MemoryViewer(QHexEdit):
     def __init__(self, parent):
         super(MemoryViewer, self).__init__(parent)
-
-
-class AsmDecoder(Decoder):
-    def decode(self, data, pos):
-        res = None
-        if pos <= data.size() - 4:
-            com = ord(data[pos]) << 24 | ord(data[pos+1]) << 16 | ord(data[pos+2]) << 8 | ord(data[pos+3])
-            res = getTextOfCommand(str(bin(com)[2:].zfill(32)))
-
-        return res if res else ""
-
-    def skipBytes(self):
-        return 4
-
